acf36ba2b9018092fe8e62137a30279612c03312
Justin Lipner <jul155@pitt.edu>
2025-01-23 08:49:50 -0500

    Modified Bus class to mirror milestone #1's instructions.

--- Bus.py
+++ Bus.py
@@ -1,12 +1,9 @@
-#  This class creates buses (nodes). Each bus has a name, voltage level, and angle.
-#  If no voltage or angle is provided, they are assumed to be zero.
+#  This class creates buses (nodes). Each bus has a name, voltage level, and index.
+
 class Bus:
-
-  def __init__(self, name: str, index: int):
-    self.index = index
+  index = 0
+  def __init__(self, name: str, base_kv: float):
     self.name = name
-    self.voltage = float
-    self.angle = float
-
-  def set_voltage(self, v: float):
-    self.voltage = v
+    self.base_kv = base_kv
+    Bus.index += 1
+    self.index = Bus.index
