24d6c15f00d8a155f4021d8ff1aba66ac54ecfe6
Justin Lipner <jul155@pitt.edu>
2025-01-25 22:30:42 -0500

    Added Bus class validation cases.

--- Bus.py
+++ Bus.py
@@ -7,3 +7,12 @@ class Bus:
     self.base_kv = base_kv
     Bus.index += 1
     self.index = Bus.index
+
+def bus_validation():
+  bus1 = Bus("Bus 1", 20)
+  bus2 = Bus("Bus 2", 230)
+  print(bus1.name, bus1.base_kv, bus1.index)
+  print(bus2.name, bus2.base_kv, bus2.index)
+  print("Bus count =", Bus.index)
+
+bus_validation()
