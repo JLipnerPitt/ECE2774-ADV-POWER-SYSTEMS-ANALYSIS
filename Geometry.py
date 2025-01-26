54382014a19ca84c5261ddf501737a9ebd18e605
Justin Lipner <jul155@pitt.edu>
2025-01-26 17:12:41 -0500

    Changed how the conductor distances are saved in __init__. Changed how Deq is calculated. Added validation cases from milestone #1.

--- Geometry.py
+++ Geometry.py
@@ -1,18 +1,26 @@
 import Bundle
+from math import prod
 
 class Geometry:
 
   def __init__(self, name: str, xa: float, ya: float, xb: float, yb: float, xc: float, yc: float):
     self.name = name
-    self.xa = xa
-    self.ya = ya
-    self.xb = xb
-    self.yb = yb
-    self.xc = xc
-    self.yc = yc
+    self.x = [xa, xb, xc]
+    self.y = [ya, yb, yc]
     self.Deq = float
-    self.calc_DEQ()
+    self.calc_Deq()
 
   # Deq = GMD = Dxy
   def calc_Deq(self):
-    self.Deq = (self.xa*self.ya*self.xb*self.yb*self.xc*self.yc)**(1/9)
+    temp = []
+    for i in range(3):
+      for k in range(3):
+        temp.append((self.x[i]*self.y[k])**(1/2))
+    self.Deq = (prod(temp))**(1/9)
+  
+def Geometry_Validation():
+  geometry1 = Geometry("Geometry 1", 0, 0, 18.5, 0, 37, 0)
+  print(geometry1.name, geometry1.x, geometry1.y)
+  print("Deq =", geometry1.Deq)
+
+Geometry_Validation()
