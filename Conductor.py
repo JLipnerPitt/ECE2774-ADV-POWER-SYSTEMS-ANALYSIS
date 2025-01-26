cecfd34271e3299cc34dd03b0369b4b924388083
Justin Lipner <jul155@pitt.edu>
2025-01-26 16:34:56 -0500

    Added radius variable to __init__ function.

--- Conductor.py
+++ Conductor.py
@@ -4,6 +4,7 @@ class Conductor:
   def __init__(self, name: str, diam: float, GMR: float, resistance: float, ampacity: float):
     self.name = name
     self.diam = diam
+    self.radius = diam/2
     self.GMR = GMR
     self.resistance = resistance
     self.ampacity = ampacity
