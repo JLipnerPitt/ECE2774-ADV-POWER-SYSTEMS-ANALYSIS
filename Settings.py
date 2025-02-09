5c82bce7dfcc37531fb368ed136a224f142eadd6
Justin Lipner <jul155@pitt.edu>
2025-02-09 11:38:00 -0500

    Added set functions for frequency and powerbase.

--- Settings.py
+++ Settings.py
@@ -4,3 +4,9 @@ class Settings:
   def __init__(self, powerbase=100, freq=60):
     self.freq = freq
     self.powerbase = powerbase
+  
+  def set_freq(self, f):
+    self.freq = f
+  
+  def set_powerbase(self, p):
+    self.powerbase = p
