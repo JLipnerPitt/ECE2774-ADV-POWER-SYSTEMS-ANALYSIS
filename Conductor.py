77ddd7a2954407339955a716b8d9ca1df1ef812b
Justin Lipner <jul155@pitt.edu>
2025-02-10 12:26:37 -0500

    Changed conductor1 information in validation tests.

--- Conductor.py
+++ Conductor.py
@@ -15,9 +15,9 @@ class Conductor:
         """
         Constructor for conductor subclass
         :param name: Name of conductor
-        :param diam: Diameter of conductor
-        :param GMR: GMR of conductor
-        :param resistance: Resistance of conductor at 50 C, 60 Hz
+        :param diam: Diameter of conductor in inches
+        :param GMR: GMR of conductor in feet
+        :param resistance: Resistance of conductor at 50°C, 60 Hz
         :param ampacity: Rated ampacity of conductor
         """
         self.name = name
@@ -31,8 +31,10 @@ class Conductor:
 # validation tests
 if __name__ == '__main__':
     from Conductor import Conductor
-    conductor1 = Conductor("Partridge", 0.642, 0.0217, 0.385, 460)
+    #conductor1 = Conductor("Partridge", 0.642, 0.0217, 0.385, 460)
+    #conductor1 = Conductor("Falcon", 1.545, 0.0520, 0.0646, 1380)
+    conductor1 = Conductor("Drake", 1.106, 0.0375, 0.1288, 900)
     print(
-        f"Name: {conductor1.name}, Diameter = {conductor1.diam}, GMR = {conductor1.GMR}, "
-        f"Radius = {conductor1.radius}, Resistance = {conductor1.resistance}, "
-        f"Ampacity = {conductor1.ampacity}")
+        f"Name: {conductor1.name}, Diameter = {conductor1.diam} in, GMR = {conductor1.GMR} ft, "
+        f"Radius = {conductor1.radius} ft, Resistance = {conductor1.resistance} Ω, "
+        f"Ampacity = {conductor1.ampacity} A")
