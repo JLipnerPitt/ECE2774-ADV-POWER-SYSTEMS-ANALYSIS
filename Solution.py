25d5fc72ade501dc100d404aa04f73fc7e6d34d2
Justin Lipner <jul155@pitt.edu>
2025-02-24 19:37:50 -0500

    Added first part of the newton-rasp algorithm.

--- Solution.py
+++ Solution.py
@@ -1,13 +1,42 @@
-import Circuit
-
+from Circuit import Circuit
+import numpy as np
+from math import sin, cos
 
 class Solution:
 
     def __init__(self, circuit: Circuit):
-        pass
+        self.circuit = circuit
 
     def newton_raph(self):
-        pass
+        iter = 100
+        N = self.circuit.count-1
+        x = self.circuit.x
+        Y = np.abs(self.circuit.Ybus)
+        theta = np.angle(self.circuit.Ybus)
+        
+        print(f"Ymag = {Y}")
+        print(f"theta = {theta}")
+        y = {"P":{}, "Q":{}}
+        P = []
+        Q = []
+    
+        for n in range(N+1):
+            for k in range(N):
+                Vk = x["V"][f"V{k+2}"]
+                Ykn = np.sum(Y[n, :], axis=0)
+                print(Ykn)
+                Vn = x["V"][f"V{n+1}"]
+                dk = x["d"][f"d{k+2}"]
+                dn = x["d"][f"d{n+1}"]
+
+                Pk = Vk*Ykn*Vn*cos(dk - dn - theta[k][n])
+                Qk = Vk*Ykn*Vn*sin(dk - dn - theta[k][n])
+                y["P"].update({f"P{k+2}": Pk})
+                y["Q"].update({f"Q{k+2}": Qk})
+        
+        print(y)
+        return y
+
 
     def fast_decoupled(self):
         pass
