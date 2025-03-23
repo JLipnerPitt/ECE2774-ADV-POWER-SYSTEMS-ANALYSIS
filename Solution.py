415a5adb6a941cea010ff5e3368617c32601f360
Justin Lipner <jul155@pitt.edu>
2025-03-23 17:17:11 -0400

    Added tolerance thresholds to the NewtonRaph and FastDecoupled algorithm classes. Refactored setup functions in both as well.

--- Solution.py
+++ Solution.py
@@ -20,42 +20,61 @@ class NewtonRaphson:
         self.slack_index = self.circuit.slack_index-1
         self.Ymag = np.abs(self.circuit.Ybus)
         self.theta = np.angle(self.circuit.Ybus)
-        self.xfull = self.circuit.flat_start_x()
-        self.yfull = None
-
+        self.tolerance = 0.001
+        self.xfull = None
         self.J1 = None
         self.J2 = None
         self.J3 = None
         self.J4 = None
 
 
-    def setup(self):
-        d = np.zeros(self.circuit.count-1)
-        V = np.ones(self.circuit.count-1-len(self.circuit.pv_indexes))
-        x = np.concatenate((d, V))
+    def set_tolerance(self, tol: float):
+        self.tolerance = tol
 
-        x_indexes = [f"d{i}" for i in np.sort(np.concatenate((self.circuit.pq_indexes, self.circuit.pv_indexes)))]
-        [x_indexes.append(f"V{i}") for i in self.circuit.pq_indexes]
-        x = pd.DataFrame(x, columns=["x"], index=x_indexes)
+
+    def x_setup(self):
+        d = np.zeros(self.circuit.count)
+        V = np.ones(self.circuit.count)
+        xfull = np.concatenate((d, V))
+
+        x_indexes = [f"d{i+1}" for i in range(self.circuit.count)]
+        [x_indexes.append(f"V{i+1}") for i in range(self.circuit.count)]
+        xfull = pd.DataFrame(xfull, index=x_indexes, columns=["x"])
+
+        x = xfull.drop(index=f"d{self.slack_index+1}").drop(
+                       index=f"V{self.slack_index+1}")
+    
+        for i in self.circuit.pv_indexes:
+            x = x.drop(index=f"V{i}")
+        
+        return xfull, x
+
+
+    def y_setup(self):
+        y = self.circuit.flat_start_y()
 
         y_indexes = [f"P{i+1}" for i in range(self.circuit.count)]
         [y_indexes.append(f"Q{i+1}") for i in range(self.circuit.count)]
-        y = pd.DataFrame(np.zeros(self.circuit.count*2), columns=["y"], index=y_indexes)
+        yfull = pd.DataFrame(np.zeros(self.circuit.count*2), columns=["y"], index=y_indexes)
+        yfull.update(y)
 
-        return x, y
-    
+        return yfull, y
+
 
     def newton_raph(self):
+        self.circuit.calc_indexes() # computes all pq and pv indexes
         iter = 50
-        x, self.yfull = self.setup()
-        y = self.circuit.flat_start_y()
         M = self.circuit.count-1
-        self.circuit.calc_indexes()
-        
+        self.xfull, x = self.x_setup()
+        yfull, y = self.y_setup()
+
         for i in range(iter):
           # step 1
           f = self.circuit.compute_power_injection(self.xfull)
           deltay = y - f
+          if np.max(deltay) < self.tolerance:
+              yfull.update(self.calc_y(self.xfull))
+              return self.xfull, yfull
 
           #step 2
           self.calc_J1_off_diag(M)
@@ -78,8 +97,8 @@ class NewtonRaphson:
           x = x + deltax
           self.xfull.update(x)
         
-        self.yfull.update(self.calc_y(self.xfull))
-        return self.xfull, self.yfull
+        yfull.update(self.calc_y(self.xfull))
+        return self.xfull, yfull
         
 
 
@@ -253,7 +272,6 @@ class NewtonRaphson:
         V = xfull[xfull.index.str.startswith('V')]
         y = np.zeros(N*2)
         indexes = np.zeros(N*2, dtype=object)
-
         for k in range(N):
             sum1 = 0
             sum2 = 0
@@ -273,7 +291,7 @@ class NewtonRaphson:
             Qk = Vk*sum2
             y[k+N] = Qk
 
-        y[np.abs(y) < 1e-4] = 0
+        y[np.abs(y) < 1e-3] = 0
         y = pd.DataFrame(y, index=indexes, columns=["y"])
         return y
 
@@ -282,10 +300,16 @@ class FastDecoupled():
     def __init__(self, circuit: Circuit):
         self.circuit = circuit
         self.slack_index = self.circuit.slack_index-1
-        self.xfull = self.circuit.flat_start_x()
         self.B = pd.DataFrame(np.imag(self.circuit.Ybus))
+        self.tolerance = 0.001
+        self.xfull = None
+        self.yfull = None
         self.J1 = None
         self.J4 = None
+
+
+    def set_tolerance(self, tol: float):
+        self.tolerance = tol
 
 
     def setup(self):
@@ -296,7 +320,50 @@ class FastDecoupled():
         Vfull = pd.DataFrame(np.ones(self.circuit.count-1), columns=["x"], index=Vfull_indexes)
         V = pd.DataFrame(np.ones(self.circuit.count-1-len(self.circuit.pv_indexes)), columns=["x"], index=V_indexes)
         d = pd.DataFrame(np.zeros(self.circuit.count-1), columns=["x"], index=d_indexes)
-        return Vfull, V, d
+        
+        y_indexes = [f"P{i+1}" for i in range(self.circuit.count)]
+        [y_indexes.append(f"Q{i+1}") for i in range(self.circuit.count)]
+        y = pd.DataFrame(np.zeros(self.circuit.count*2), columns=["y"], index=y_indexes)
+
+        x_indexes = [f"d{i+1}" for i in range(self.circuit.count)]
+        [x_indexes.append(f"V{i+1}") for i in range(self.circuit.count)]
+        xfull = np.concatenate((np.zeros(self.circuit.count), 
+                                np.ones(self.circuit.count)))
+        xfull = pd.DataFrame(xfull, index=x_indexes, columns=["x"])
+
+        return Vfull, V, d, y, xfull
+
+
+    def fast_decoupled(self):
+        iter = 75
+        Vfull, V, d, self.yfull, self.xfull = self.setup()
+        y = self.circuit.flat_start_y()
+        self.calc_J1(Vfull)
+        self.calc_J4(V)
+
+        for i in range(iter):
+          # step 1
+          f = self.circuit.compute_power_injection(self.xfull)
+          deltay = y - f
+          if np.max(deltay) < self.tolerance:
+              self.yfull.update(self.calc_y(self.xfull))
+              return self.xfull, self.yfull
+          
+          P = deltay[deltay.index.str.startswith('P')]
+          Q = deltay[deltay.index.str.startswith('Q')]
+
+          # step 2
+          deltad = np.linalg.solve(self.J1, P.to_numpy())
+          deltaV = np.linalg.solve(self.J4, Q.to_numpy())
+
+          #step 3
+          d = d + deltad
+          V = V + deltaV
+          self.xfull.update(d)
+          self.xfull.update(V)
+        
+        self.yfull.update(self.calc_y(self.xfull))
+        return self.xfull, self.yfull
     
 
     def calc_J1(self, V):
@@ -315,33 +382,38 @@ class FastDecoupled():
         V = np.diag(V.to_numpy().flatten())
         self.J4 = -np.matmul(np.abs(V), B.to_numpy())
 
-
-    def fast_decoupled(self):
-        iter = 75
-        Vfull, V, d = self.setup()
-        y = self.circuit.flat_start_y()
-        self.calc_J1(Vfull)
-        self.calc_J4(V)
-    
-        for i in range(iter):
-          # step 1
-          f = self.circuit.compute_power_injection(self.xfull)
-          deltay = y - f
-          P = deltay[deltay.index.str.startswith('P')]
-          Q = deltay[deltay.index.str.startswith('Q')]
-
-          # step 2
-          deltad = np.linalg.solve(self.J1, P.to_numpy())
-          deltaV = np.linalg.solve(self.J4, Q.to_numpy())
-
-          #step 3
-          d = d + deltad
-          V = V + deltaV
-          self.xfull.update(d)
-          self.xfull.update(V)
         
-        return self.xfull
+    def calc_y(self, xfull):
+        N = self.circuit.count
+        d = xfull[xfull.index.str.startswith('d')]
+        V = xfull[xfull.index.str.startswith('V')]
+        y = np.zeros(N*2)
+        indexes = np.zeros(N*2, dtype=object)
+        Ymag = np.abs(self.circuit.Ybus)
+        theta = np.angle(self.circuit.Ybus)
+        for k in range(N):
+            sum1 = 0
+            sum2 = 0
+            indexes[k] = f"P{k+1}"
+            indexes[k+N] = f"Q{k+1}"
+            for n in range(N):
+                Ykn = Ymag[k, n]
+                Vn = float(V.iloc[n, 0])
+                dk = float(d.iloc[k, 0])
+                dn = float(d.iloc[n, 0])
+                sum1 += Ykn*Vn*cos(dk - dn - theta[k, n])
+                sum2 += Ykn*Vn*sin(dk - dn - theta[k, n])
+            
+            Vk = float(V.iloc[k, 0])
+            Pk = Vk*sum1
+            y[k] = Pk
+            Qk = Vk*sum2
+            y[k+N] = Qk
 
+        y[np.abs(y) < 1e-3] = 0
+        y = pd.DataFrame(y, index=indexes, columns=["y"])
+        return y
+    
 
 class DCPowerFlow():
     def __init__(self, circuit: Circuit):
