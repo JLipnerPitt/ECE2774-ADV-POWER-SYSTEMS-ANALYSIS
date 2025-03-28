070099eadca0e48727b4747d51b7e7aad7d0bed5
Justin Lipner <jul155@pitt.edu>
2025-03-28 10:50:48 -0400

    Updated to work with the new changes made to other classes.

--- Validations.py
+++ Validations.py
@@ -1,49 +1,47 @@
 import numpy as np
 
-from Circuit import Circuit, Faults
+from Circuit import Circuit, ThreePhaseFaults
 from Settings import settings
 from Tools import read_excel, compare, read_jacobian, display_jacobian
 
 
 def CreateFivePowerBusSystem():
-    settings.set_powerbase(100e6)
     circ = Circuit("Example_6.9")
 
-    circ.add_bus("bus1", 15e3)
-    circ.add_bus("bus2", 345e3)
-    circ.add_bus("bus3", 15e3)
-    circ.add_bus("bus4", 345e3)
-    circ.add_bus("bus5", 345e3)
+    circ.add_bus("bus1", 15)
+    circ.add_bus("bus2", 345)
+    circ.add_bus("bus3", 15)
+    circ.add_bus("bus4", 345)
+    circ.add_bus("bus5", 345)
 
-    circ.add_transformer("T1", circ.buses["bus1"], circ.buses["bus5"], 400e6, 8.024, 13.33333333)
-    circ.add_transformer("T2", circ.buses["bus3"], circ.buses["bus4"], 800e6, 8.024, 13.33333333)
+    circ.add_transformer("T1", circ.buses["bus1"], circ.buses["bus5"], 400, 8.024, 13.33333333)
+    circ.add_transformer("T2", circ.buses["bus3"], circ.buses["bus4"], 800, 8.024, 13.33333333)
 
     circ.add_tline_from_parameters("L1", circ.buses["bus2"], circ.buses["bus4"], R=0.009, X=0.100, B=1.72)
     circ.add_tline_from_parameters("L2", circ.buses["bus2"], circ.buses["bus5"], R=0.0045, X=0.05, B=0.88)
     circ.add_tline_from_parameters("L3", circ.buses["bus5"], circ.buses["bus4"], R=0.00225, X=0.025, B=0.44)
 
     circ.add_generator("Gen1", "bus1", 1, 0, 0.045)
-    circ.add_generator("Gen2", "bus3", 1, 520e6, 0.0225)
+    circ.add_generator("Gen2", "bus3", 1, 520, 0.0225)
 
-    circ.add_load("Load1", "bus2", 800e6, 280e6)
-    circ.add_load("Load2", "bus3", 80e6, 40e6)
+    circ.add_load("Load1", "bus2", 800, 280)
+    circ.add_load("Load2", "bus3", 80, 40)
     return circ
 
 
 def CreateSevenPowerBusSystem():
-    settings.set_powerbase(100e6)
     circ = Circuit("Example_7bus")
 
-    circ.add_bus("bus1", 20e3)
-    circ.add_bus("bus2", 230e3)
-    circ.add_bus("bus3", 230e3)
-    circ.add_bus("bus4", 230e3)
-    circ.add_bus("bus5", 230e3)
-    circ.add_bus("bus6", 230e3)
-    circ.add_bus("bus7", 18e3)
+    circ.add_bus("bus1", 20)
+    circ.add_bus("bus2", 230)
+    circ.add_bus("bus3", 230)
+    circ.add_bus("bus4", 230)
+    circ.add_bus("bus5", 230)
+    circ.add_bus("bus6", 230)
+    circ.add_bus("bus7", 18)
 
-    circ.add_transformer("T1", circ.buses["bus1"], circ.buses["bus2"], 125e6, 8.5, 10)
-    circ.add_transformer("T2", circ.buses["bus6"], circ.buses["bus7"], 200e6, 10.5, 12)
+    circ.add_transformer("T1", circ.buses["bus1"], circ.buses["bus2"], 125, 8.5, 10)
+    circ.add_transformer("T2", circ.buses["bus6"], circ.buses["bus7"], 200, 10.5, 12)
 
     circ.add_conductor("Partridge", 0.642, 0.0217, 0.385, 460)
     circ.add_geometry("Geometry7bus", [0, 19.5, 39], [0, 0, 0])
@@ -56,12 +54,12 @@ def CreateSevenPowerBusSystem():
     circ.add_tline_from_geometry("L5", circ.buses["bus5"], circ.buses["bus6"], circ.bundles["Bundle7bus"], circ.geometries["Geometry7bus"], 10)
     circ.add_tline_from_geometry("L6", circ.buses["bus4"], circ.buses["bus5"], circ.bundles["Bundle7bus"], circ.geometries["Geometry7bus"], 35)
 
-    circ.add_generator("Gen1", "bus1", 1, 0, 0.12)
-    circ.add_generator("Gen2", "bus7", 1, 200e6, 0.12)
+    circ.add_generator("Gen1", "bus1", 1, 200, 0.12)
+    circ.add_generator("Gen2", "bus7", 1, 200, 0.12)
 
-    circ.add_load("Load1", "bus3", 110e6, 50e6)
-    circ.add_load("Load2", "bus4", 100e6, 70e6)
-    circ.add_load("Load3", "bus5", 100e6, 65e6)
+    circ.add_load("Load1", "bus3", 110, 50)
+    circ.add_load("Load2", "bus4", 100, 70)
+    circ.add_load("Load3", "bus5", 100, 65)
 
     return circ
 
@@ -74,17 +72,18 @@ def FivePowerBusSystemValidation():
     NewtonRaphValidation(circ)
     FastDecoupledValidation(circ)
     DCPowerFlowValidation(circ)
-    FaultsValidation(circ)
+    ThreePhaseFaultsValidation(circ)
 
 
 def SevenPowerBusSystemValidation():
     circ = CreateSevenPowerBusSystem()
+    #circ.change_slack("bus1", "bus7")
     ImpedanceValidation(circ)
     YbusValidation(circ, r"Excel_Files\sevenpowerbus_system.xlsx")
     NewtonRaphValidation(circ)
     FastDecoupledValidation(circ)
     DCPowerFlowValidation(circ)
-    FaultsValidation(circ)
+    ThreePhaseFaultsValidation(circ)
 
 
 def ImpedanceValidation(circ: Circuit):
@@ -135,8 +134,13 @@ def DCPowerFlowValidation(circ: Circuit):
     print()
 
 
-def FaultsValidation(circ: Circuit):
-    faults = Faults(circ)
-    print("Fault Validations:")
-    print("Zbus=")
-    print(faults.faultZbus)
+def ThreePhaseFaultsValidation(circ: Circuit):
+    faults = ThreePhaseFaults(circ, 1, 1.0)
+    print("ThreePhase Fault Validations:")
+    pwrworld = read_excel(r"Excel_Files\7bus_positive_sequence_Ybus_matrix.xlsx")
+    compare(faults.faultYbus, pwrworld)
+    fault_voltages = faults.calc_fault_voltages()
+    print()
+    print("fault current =", faults.I_fn)
+    print("fault voltages:")
+    print(fault_voltages)
