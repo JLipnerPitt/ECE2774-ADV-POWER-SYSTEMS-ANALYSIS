a1280e185a72a62de4ea29b0c5e4f81ca36a991e
Justin Lipner <jul155@pitt.edu>
2025-04-16 09:45:08 -0400

    Added set_power method.

--- Bus.py
+++ Bus.py
@@ -8,25 +8,17 @@ Date: 2025-01-23
 
 
 class Bus:
-    """
-    Bus class to hold bus information
-    """
 
     def __init__(self, name: str, base_kv: float, index: int):
-        """
-        Bus object constructor
-        :param name: Name of bus
-        :param base_kv: Rated bus voltage
-        """
         self.name = name
         self.base_kv = base_kv*1e3
         self.index = index
-        self.Vpu = 1
-        self.V = base_kv
-        self.angle = 0
-        self.real_power = 0
-        self.reactive_power = 0
-        self.type = "PQ"
+        self.Vpu = 1 # per unit voltage
+        self.V = base_kv # actual voltage.
+        self.angle = 0.0
+        self.real_power = 0.0
+        self.reactive_power = 0.0
+        self.type = "PQ" # bus type
     
 
     def set_bus_v(self, v: float):
@@ -34,12 +26,17 @@ class Bus:
         self.V = self.base_kv*v
     
 
-    def set_angle(self, d: float):
-        self.angle = d
+    def set_angle(self, a: float):
+        self.angle = a
 
 
-    def set_type(self, type: str):
-        self.type = type
+    def set_type(self, t: str):
+        self.type = t
+    
+
+    def set_power(self, real: float, reactive: float):
+        self.real_power = real
+        self.reactive_power = reactive
 
 
 # validation tests
